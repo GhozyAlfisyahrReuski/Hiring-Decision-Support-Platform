d7873418af0e21addb26bdb9e71e7123151c259b
GhozyAlfisyahrReuski <ghozy1978@gmail.com>
2025-08-13 19:13:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -15,14 +15,15 @@ with st.sidebar:
     st.write(f'You are in: {page} page')
 
     st.write('## About')
-# magic
-'''
-The **CitoConnect Hiring Decision Support Platform** helps companies make **data-driven hiring decisions**.  
-It predicts a candidate’s hiring outcome, calculates an overall score, and highlights the most important factors.  
+    # magic
+    '''
+    The **CitoConnect Hiring Decision Support Platform** helps companies make **data-driven hiring decisions**.  
+    It predicts a candidate’s hiring outcome, calculates an overall score, and highlights the most important factors.  
 
-Designed to improve efficiency, reduce bias, and provide fair, explainable results,  
-CitoConnect empowers HR teams to hire **smarter and faster**.
-'''
+    Designed to improve efficiency, reduce bias, and provide fair, explainable results,  
+    CitoConnect empowers HR teams to hire **smarter and faster**.
+    '''
+
 
 
 # bagian luar sidebar
